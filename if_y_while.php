0e31669abe870df9c005011035d6b8a8ef29e47e
CarlosZaltzman <czaltzman@yahoo.com.ar>
2017-05-23 22:57:02 -0300

    Add files via upload

---
+++ if_y_while.php
@@ -0,0 +1,22 @@
+﻿<?php
+$a = 30;
+
+if ($a > 10){
+  echo("Mayor que 10<br>");
+}
+
+if ($a>50){
+  echo("Mayor que 50<br>");
+}
+else{
+ echo("Menor o igual que 50<br>");
+}
+
+$a=1;
+$suma=0;
+while($a<=10){
+  $suma=$suma+$a;
+  $a=$a+1; 	 
+}
+echo $suma
+?>
